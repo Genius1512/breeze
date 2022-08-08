ead5500090e9fd9430304a86c7d40ccfc9f96083
Silvan Schmidt <silvan.genius@gmail.com>
2022-08-08 09:03:18 +0200

    Cleanup sandbox

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,16 +1,12 @@
-from breeze import builtin
 from breeze.core import Game
 
 
 def main():
     game = Game("Game")
 
-    dbg_game_object = game.add_game_object(builtin.game_objects.DebugGameObject("Player"))
-    dbg_component = dbg_game_object.add_component(builtin.components.DebugComponent())
-
     try:
-        for _ in range(10):
-            game.update()
+        while game.update():
+            pass
     except KeyboardInterrupt:
         pass
 
